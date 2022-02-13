f92b09b7cd5d0b3789d0841847951bff87d67939
Key <34916202+Lumnca@users.noreply.github.com>
2022-02-13 21:34:40 +0800

    标签修改

--- 前端部分/移动端Vux代码/components/Games.vue
+++ 前端部分/移动端Vux代码/components/Games.vue
@@ -13,8 +13,15 @@
             <flexbox-item>
               <div class="flex-demo">
                 <h4>{{game.name}}</h4>
-                <div>类型:{{game.label}}</div>
-                <div>平台:{{devs[game.devType]}}</div>
+              <div>
+                类型: <span class="lab" v-for="lab in JSON.parse(game.label) " :key="lab">{{lab}}</span>
+                </div> 
+                <div >平台:{{devs[game.devType]}}
+                  <span style="font-size:14px;">
+                     &nbsp;<svg t="1644746223581" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2084" width="14" height="14"><path d="M857.28 344.992h-264.832c12.576-44.256 18.944-83.584 18.944-118.208 0-78.56-71.808-153.792-140.544-143.808-60.608 8.8-89.536 59.904-89.536 125.536v59.296c0 76.064-58.208 140.928-132.224 148.064l-117.728-0.192A67.36 67.36 0 0 0 64 483.04V872c0 37.216 30.144 67.36 67.36 67.36h652.192a102.72 102.72 0 0 0 100.928-83.584l73.728-388.96a102.72 102.72 0 0 0-100.928-121.824zM128 872V483.04c0-1.856 1.504-3.36 3.36-3.36H208v395.68H131.36A3.36 3.36 0 0 1 128 872z m767.328-417.088l-73.728 388.96a38.72 38.72 0 0 1-38.048 31.488H272V476.864a213.312 213.312 0 0 0 173.312-209.088V208.512c0-37.568 12.064-58.912 34.72-62.176 27.04-3.936 67.36 38.336 67.36 80.48 0 37.312-9.504 84-28.864 139.712a32 32 0 0 0 30.24 42.496h308.512a38.72 38.72 0 0 1 38.048 45.888z" p-id="2085"></path></svg>
+                     ({{game.good}})
+                  </span>
+                </div>
               </div>
             </flexbox-item>
           </flexbox>
@@ -26,6 +33,9 @@
 </template>
 
 <script>
+import { dateFormat } from 'vux'
+
+
 import {
   Grid,
   GridItem,
@@ -70,6 +80,9 @@ export default {
     };
   },
   methods: {
+    dateFormat(date,str){
+      return dateFormat(date,str);
+    },
     onItemClick() {
       console.log("on item click");
     },
@@ -78,7 +91,7 @@ export default {
         path: "/game", //跳转的路径
         query: {
           //路由传参时push和query搭配使用 ，作用时传递参数
-          game:game
+          game: game
         }
       });
     }
@@ -108,5 +121,18 @@ export default {
 }
 .game-item {
   padding: 0px;
+}
+.lab {
+  text-align: center;
+  color: #fff;
+  background-color: #b2c0bf;
+  border-radius: 4px;
+  background-clip: padding-box;
+  padding: 2px 6px;
+  margin-left: 6px;
+  font-size: 12px;
+}
+.icon{
+
 }
 </style>
